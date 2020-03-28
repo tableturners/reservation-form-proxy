1ccfd27fdc7b0031b3d159b4be96d7564a20c19c
Aizam Jigjidsuren <aizamjig@gmail.com>
2020-03-28 11:20:04 -0700

    Set up Docker container.

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
+const httpProxy = require('http-proxy');
+const apiProxy = httpProxy.createProxyServer();
+
 
 const app = express();
 const PORT = 3000;
@@ -8,4 +11,17 @@ const PORT = 3000;
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 app.listen(PORT, () => console.log(`Server is listening on port ${PORT}.`));
+
+const reservationTarget = 'http://ec2-18-221-246-97.us-east-2.compute.amazonaws.com/';
+
+app.all('/api/restaurants', (req, res) => {
+    console.log('redirecting to server 1');
+    apiProxy.web(req, res, { target: reservationTarget });
+  });
+
+app.all('/api/restaurants/:id', (req, res) => {
+    console.log('redirecting to server 1');
+    apiProxy.web(req, res, { target: reservationTarget });
+});
